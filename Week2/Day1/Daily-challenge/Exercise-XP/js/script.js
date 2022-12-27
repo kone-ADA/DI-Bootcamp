df6b916fa9e30b47411a77e30918a4300a08d552
kone-yaya-dev <yaya.kone@futurafric.com>
2022-12-27 12:54:14 +0000

    first commit for Week 2

---
+++ Week2/Day1/Daily-challenge/Exercise-XP/js/script.js
@@ -0,0 +1,28 @@
+// EXERCICE 1
+const fruits = ["Banana", "Apples", "Oranges", "Blueberries"];
+
+//Remove Banana from the array
+fruits.shift();
+console.log(fruits) //["Apples", "Oranges", "Blueberries"]
+
+// Sort the array in alphabetical order.
+fruits.Sort()
+console.log(fruits) //['Apples', 'Banana', 'Blueberries', 'Oranges']
+
+// Add “Kiwi” to the end of the array
+fruits.push("kiwi")
+console.log(fruits) //['Apples', 'Banana', 'Blueberries', 'Oranges', 'kiwi']
+
+//Remove “Apples” from the array. Don’t use the same method as in part 1.
+fruits.splice(0,1)
+console.log(fruits) //['Banana', 'Blueberries', 'Oranges', 'kiwi']
+
+//Sort the array in reverse order
+fruits.reverse()
+console.log(fruits) //['kiwi','Oranges', 'Blueberries', 'Banana']
+
+//EXERCICE 2
+
+const moreFruits = ["Banana", ["Apples", ["Oranges"], "Blueberries"]];
+console.log(moreFruits[1][1][0])
+
